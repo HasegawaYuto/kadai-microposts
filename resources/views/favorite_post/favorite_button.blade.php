39f437da667786f9ec735b729f03e6fe00e5c5da
yutohasegawa <buenas_tardes@re.commufa.jp>
2017-08-30 09:48:45 +0000

    favorite_button '->id' to '->id'

--- resources/views/favorite_post/favorite_button.blade.php
+++ resources/views/favorite_post/favorite_button.blade.php
@@ -1,10 +1,10 @@
 @if (Auth::user()->id != $user->id) 
     @if (Auth::user()->is_favorite($micropost->id))
-        {!! Form::open(['route' => ['micropost.unfavorite', $user->id], 'method' => 'delete']) !!}
+        {!! Form::open(['route' => ['micropost.unfavorite', $micropost->id], 'method' => 'delete']) !!}
             {!! Form::submit('Unfavorite', ['class' => "btn btn-danger btn-xs"]) !!}
         {!! Form::close() !!}
     @else
-        {!! Form::open(['route' => ['micropost.favorite', $user->id]]) !!}
+        {!! Form::open(['route' => ['micropost.favorite', $micropost->id]]) !!}
             {!! Form::submit('Favorite', ['class' => "btn btn-primary btn-xs"]) !!}
         {!! Form::close() !!}
     @endif
